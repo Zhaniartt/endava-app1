3fcf7fbba02bad027ffea92984c70dc3041fd723
Zhani Tovmasyan <Zhani.Tovmasyan@endava.com>
2020-03-27 14:55:31 +0200

    Updating api service file

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private SERVER_URL = "http://localhost:3000";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 }
